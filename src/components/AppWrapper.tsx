c9f8c062aa5bdb7adca6303a07d2ae358102ccbc
ewetchy <elisabeth.wetchy@noc.ac.uk>
2025-03-27 12:13:45 +0000

    Downgrade Next and React for BODC navbar

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { ReactNode } from 'react'
 
+import { BodcNavbar } from '@bodc/navbar'
 import { HeroUIProvider } from '@heroui/react'
 import classNames from 'classnames'
 import { useRouter } from 'next/navigation'
@@ -22,6 +23,20 @@ export const AppWrapper = ({ children }: {children: ReactNode}) => {
       >
         Skip to content
       </a>
+      <BodcNavbar
+        content={
+          <div
+            style={{
+              display: 'flex',
+              marginTop: 'auto',
+              marginBottom: 'auto',
+              paddingRight: '15px',
+              gap: '10px'
+            }}
+          >
+          </div>
+        }
+      />
       <main className='container mt-6' id='app-container'>
         <>
           {children}
